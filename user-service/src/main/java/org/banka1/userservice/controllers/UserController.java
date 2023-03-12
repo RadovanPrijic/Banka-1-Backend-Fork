cfe5cda2e900a2c438ddfa06fe8b0d0f2650eba8
itodorovic5019rn <itodorovic5019rn@raf.rs>
2023-03-12 14:08:17 +0100

    swagger controller annotations

--- user-service/src/main/java/org/banka1/userservice/controllers/UserController.java
+++ user-service/src/main/java/org/banka1/userservice/controllers/UserController.java
@@ -1,5 +1,10 @@
 package org.banka1.userservice.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.banka1.userservice.domains.dtos.login.LoginRequest;
 import org.banka1.userservice.domains.dtos.login.LoginResponse;
@@ -13,6 +18,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.awt.print.Book;
 
 @RestController
 @RequestMapping(path = "/api/users")
@@ -36,6 +42,11 @@ public class UserController {
         return ResponseEntity.ok(new LoginResponse(jwtUtil.generateToken(userService.findUserByEmail(loginRequest.getEmail()))));
     }
 
+    @Operation(summary = "Get users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found users",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class)) })})
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<?> getUsers(@RequestBody UserFilterRequest filterRequest,
@@ -45,40 +56,91 @@ public class UserController {
         return ResponseEntity.ok(userService.getUsers(filterRequest, page, size));
     }
 
+    @Operation(summary = "Get a user by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the user",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.findUserById(id));
     }
 
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid email or jmbg supplied",
+                    content = @Content)})
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
         return ResponseEntity.ok(userService.createUser(userCreateDto));
     }
 
+    @Operation(summary = "Update user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid password supplied",
+                    content = @Content)})
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> updateUser(@RequestBody UserUpdateDto userUpdateDto, @PathVariable Long id) {
         return ResponseEntity.ok(userService.updateUser(userUpdateDto, id));
     }
 
+    @Operation(summary = "About me")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "About me",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content)})
     @GetMapping("/my-profile")
     public ResponseEntity<?> aboutMe() {
         return ResponseEntity.ok(userService.returnUserProfile());
     }
 
+    @Operation(summary = "Update my profile")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update my profile",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content)})
     @PutMapping("/my-profile/update")
     public ResponseEntity<?> updateMyself(@RequestBody UserUpdateMyProfileDto userUpdateMyProfileDto) {
         return ResponseEntity.ok(userService.updateUserProfile(userUpdateMyProfileDto));
     }
 
+    @Operation(summary = "Reset password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Password reset",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid password supplied",
+                    content = @Content)})
     @PostMapping("/reset-password/{id}")
     public ResponseEntity<?> resetPassword(@RequestBody PasswordDto passwordDto, @PathVariable Long id) {
         userService.resetUserPassword(passwordDto, id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Forgot password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email sent",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content)})
     @GetMapping(value = "/forgot-password")
     public ResponseEntity<?> forgotPassword(@RequestParam String email) {
         userService.forgotPassword(email);
