3f5d1fe5833128f31a2c736667dc19e59c88541c
Nikola <nikola.dubak@hotmail.com>
2023-03-13 20:47:38 +0100

    Postavljeno slanje mailova da bude asinhrono da se zaobiđe čekanje

--- user-service/src/main/java/org/banka1/userservice/services/EmailService.java
+++ user-service/src/main/java/org/banka1/userservice/services/EmailService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
