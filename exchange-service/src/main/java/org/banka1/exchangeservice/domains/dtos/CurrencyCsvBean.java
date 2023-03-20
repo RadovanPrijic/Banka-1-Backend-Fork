bc3486063368557f3e2f31ac9bc45905ae244758
RadovanPrijic <rprijic3619rn@raf.rs>
2023-03-20 18:14:58 +0100

    Dodat jedan test.

--- exchange-service/src/main/java/org/banka1/exchangeservice/domains/dtos/CurrencyCsvBean.java
+++ exchange-service/src/main/java/org/banka1/exchangeservice/domains/dtos/CurrencyCsvBean.java
@@ -2,10 +2,12 @@ package org.banka1.exchangeservice.domains.dtos;
 
 import com.opencsv.bean.CsvBindByName;
 import com.opencsv.bean.CsvBindByPosition;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class CurrencyCsvBean {
 
