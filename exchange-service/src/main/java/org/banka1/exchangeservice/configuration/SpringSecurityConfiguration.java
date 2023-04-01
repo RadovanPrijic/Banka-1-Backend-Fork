67566591d7dee6c9ce07692ef9f1d30230e889a1
urosn <unikolic2619rn@raf.rs>
2023-04-01 14:09:31 +0200

    Filter for exchabge service

--- exchange-service/src/main/java/org/banka1/exchangeservice/configuration/SpringSecurityConfiguration.java
+++ exchange-service/src/main/java/org/banka1/exchangeservice/configuration/SpringSecurityConfiguration.java
@@ -1,4 +1,56 @@
 package org.banka1.exchangeservice.configuration;
 
+import org.banka1.exchangeservice.errors.MyAccessDeniedHandler;
+import org.banka1.exchangeservice.filters.CustomAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SpringSecurityConfiguration {
+
+    @Value("${jwt.secret}")
+    private String jwtSecretKey;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and().authorizeHttpRequests()
+                .requestMatchers(new AntPathRequestMatcher("/login/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/users/reset-password/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/users/forgot-password/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/v3/**")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new CustomAuthorizationFilter(jwtSecretKey), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().accessDeniedHandler(new MyAccessDeniedHandler()).and()
+                .httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
