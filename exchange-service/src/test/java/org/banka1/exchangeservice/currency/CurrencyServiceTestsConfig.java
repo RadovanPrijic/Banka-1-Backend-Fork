546ef68ee24b2531b4a097d2748c682073eef0f6
RadovanPrijic <rprijic3619rn@raf.rs>
2023-04-02 20:05:02 +0200

    Mala ispravka

--- exchange-service/src/test/java/org/banka1/exchangeservice/currency/CurrencyServiceTestsConfig.java
+++
@@ -1,9 +0,0 @@
-package org.banka1.exchangeservice.currency;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest
-public class CurrencyServiceTestsConfig {
-}
--- exchange-service/src/test/resources/features.currency/currency-service-tests.feature
+++
@@ -1,9 +0,0 @@
-Feature: Servis za upravljanje sa valutama
-
-  Ovaj servis sluzi da bi se valute perzistirale u bazu podataka.
-
-  Scenario: Dodavanje valuta
-    When dodaju se nove valute
-    Then valute se sacuvaju u bazi podataka
-
-
