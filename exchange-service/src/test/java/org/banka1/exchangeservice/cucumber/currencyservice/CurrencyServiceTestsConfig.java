071ff79d75e5c817e6ffb30da7dcf12291c0f11a
RadovanPrijic <rprijic3619rn@raf.rs>
2023-04-03 14:47:52 +0200

    Ispravka

--- exchange-service/src/test/java/org/banka1/exchangeservice/cucumber/currencyservice/CurrencyServiceTestsConfig.java
+++ exchange-service/src/test/java/org/banka1/exchangeservice/cucumber/currencyservice/CurrencyServiceTestsConfig.java
@@ -3,13 +3,13 @@ package org.banka1.exchangeservice.cucumber.currencyservice;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest
 @AutoConfigureMockMvc
-@Profile("local")
-@TestPropertySource(value = "/application-test_it-local.properties")
+@TestPropertySource(value = "/application-test_it.properties")
+@ActiveProfiles("test_it")
 public class CurrencyServiceTestsConfig {
 }
