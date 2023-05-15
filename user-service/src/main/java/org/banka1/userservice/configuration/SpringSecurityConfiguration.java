57da6e1790521d0ef692819945bcf28cd792ff54
Andrija <andrijadjelic@gmail.com>
2023-05-15 18:34:11 +0200

    Dodata anotacija da bi radila anotacija @PreAuthorize()

--- user-service/src/main/java/org/banka1/userservice/configuration/SpringSecurityConfiguration.java
+++ user-service/src/main/java/org/banka1/userservice/configuration/SpringSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.banka1.userservice.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @AllArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
