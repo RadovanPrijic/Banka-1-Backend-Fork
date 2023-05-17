6662fc978cec3abeab49b71b7129a965c46986b7
VukV <vuk.vukovic.2000@gmail.com>
2023-05-17 14:29:29 +0200

    cors

--- contracts-service/src/service.ts
+++ contracts-service/src/service.ts
@@ -1,5 +1,6 @@
 import express, {json, NextFunction, Request, Response} from 'express';
 import {connect} from "./mongo-db";
+import cors from 'cors'
 import contractsRoute from './routes/contract.route';
 import finalisedContractRoute from "./routes/finalised-contract.route";
 
@@ -13,6 +14,8 @@ connect()
     .catch(() => console.log('Connection to MongoDB failed.'));
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/api/contracts', contractsRoute);
 app.use('/api/contracts/finalised', finalisedContractRoute);
 
