7ffa38cc25375673328441eb49650860b020ebb2
Lazar Dejanovic <lazzar99@gmail.com>
2023-05-21 12:17:35 +0200

    implementing users contracts

--- user-service/src/main/java/org/banka1/userservice/repositories/UserListingRepository.java
+++ user-service/src/main/java/org/banka1/userservice/repositories/UserListingRepository.java
@@ -1,14 +1,18 @@
 package org.banka1.userservice.repositories;
 
+import org.banka1.userservice.domains.entities.ListingType;
 import org.banka1.userservice.domains.entities.UserListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface UserListingRepository extends JpaRepository<UserListing, Long> {
 
     List<UserListing> findByUser_Id(Long id);
+
+    List<UserListing> findAllBySymbolInAndListingTypeAndUser_Id(Set<String> symbols, ListingType listingType, Long userId);
 
 }
