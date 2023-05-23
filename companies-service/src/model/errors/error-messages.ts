12954b5cb3669eb54fa18d4829c7ca8de5b3549c
VukV <vuk.vukovic.2000@gmail.com>
2023-05-23 12:11:02 +0200

    company route

--- companies-service/src/model/errors/error-messages.ts
+++ companies-service/src/model/errors/error-messages.ts
@@ -3,4 +3,10 @@ export class ErrorMessages{
     public static userNotLoggedInError: string = "Korinsik nije ulogovan.";
 
     public static unauthorizedAccessError: string = "Nedozvoljen pristup."
+
+    public static companiesGetError: string = "Greška prilikom dohvatanja kompanija.";
+
+    public static companiesCreateError: string = "Greška prilikom kreiranja kompanije.";
+
+    public static companiesUpdateError: string = "Greška prilikom izmene kompanije.";
 }
--- companies-service/src/routes/company.route.ts
+++ companies-service/src/routes/company.route.ts
@@ -1,9 +1,86 @@
 import express from 'express';
+import {authToken} from "../middleware/auth.middleware";
+import Company from "../model/companies/company.model";
+import {ErrorMessages} from "../model/errors/error-messages";
 
 
 const router = express.Router();
 
-//todo
 
+router.get('/', authToken, async (req, res) => {
+    try {
+        let name = req.query['name'] || "";
+        let registrationNumber = req.query['registrationNumber'] || "";
+        let taxNumber = req.query['taxNumber'] || "";
+
+        let searchQuery = {
+            name: new RegExp(String(name), 'i'),
+            registrationNumber: new RegExp(String(registrationNumber), 'i'),
+            taxNumber: new RegExp(String(taxNumber), 'i')
+        }
+
+        const companies = await Company.find(searchQuery);
+        res.json(companies);
+    } catch (error) {
+        console.error(ErrorMessages.companiesGetError, error);
+        res.status(500).send({ message: ErrorMessages.companiesGetError });
+    }
+});
+
+
+router.get('/:companyId', authToken, async (req, res) => {
+    try {
+        const companies = await Company.findById(req.params['companyId']);
+        res.json(companies);
+    } catch (error) {
+        console.error(ErrorMessages.companiesGetError, error);
+        res.status(500).send({ message: ErrorMessages.companiesGetError });
+    }
+});
+
+
+router.post('/', authToken, async (req, res) => {
+    try {
+        const newCompany = new Company({
+            name: req.body.name,
+            registrationNumber: req.body.registrationNumber,
+            taxNumber: req.body.taxNumber,
+            activityCode: req.body.activityCode,
+            address: req.body.address
+        });
+
+        const savedContract = await newCompany.save();
+        res.json(savedContract);
+    } catch (error) {
+        console.error(ErrorMessages.companiesCreateError, error);
+        res.status(500).send({ message: ErrorMessages.companiesCreateError });
+    }
+});
+
+
+router.put('/:companyId', authToken, async (req, res) => {
+    try {
+        let companyId = req.params['companyId'];
+        const company = await Company.findById(companyId);
+        if(company){
+            const update = {
+                name: req.body.name,
+                activityCode: req.body.activityCode,
+                address: req.body.address
+            }
+
+            await Company.findByIdAndUpdate(companyId, update);
+            res.status(200).send();
+        }
+    } catch (error) {
+        console.error(ErrorMessages.companiesUpdateError, error);
+        res.status(500).send({ message: ErrorMessages.companiesUpdateError });
+    }
+});
+
+
+router.delete('/:contractId', authToken, async (req, res) => {
+    //TODO
+});
 
 export default router;
